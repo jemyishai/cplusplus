cfdd65d3f425be273c128c24f3f6be03890e7afc
jesse <jesse.moskowitz@gmail.com>
2020-08-11 07:59:40 -0400

    different error codes

---
+++ error_code.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(){
+
+std::cout << "Testing a different error return" << std::endl;
+std::cout << "Talking to Linux differently" <<std::endl;
+
+return 13;
+
+}
---
+++ success.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+
+std::cout << "Testing error return" << std::endl;
+
+return 0;
+
+}
