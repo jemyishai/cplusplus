683fb6f8a7b7d852f99e0d94c7862bdcbd342ee5
jesse <jesse.moskowitz@gmail.com>
2020-08-13 09:40:43 -0400

    wraparound

---
+++ wraparound_short_max.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+ short myVal = 32767;
+ 
+ cout << "The value of myVal is " << myVal << endl;
+ 
+ myVal++;
+ 
+ cout << "After adding 1 to myVal, the new value is " << myVal << endl;
+ 
+ return 0;
+ 
+ }
+
+
