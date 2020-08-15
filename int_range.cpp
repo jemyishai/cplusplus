ff96134908992c72366ab09040dc42c8b175701c
jesse <jesse.moskowitz@gmail.com>
2020-08-15 08:25:09 -0400

    climits library

---
+++ int_range.cpp
@@ -0,0 +1,53 @@
+#include <iostream> 
+#include <climits>
+
+using namespace std;
+
+int main(){
+	
+	cout << "char ranges from " << CHAR_MIN;
+	cout << " up to " << CHAR_MAX;
+	
+	cout << endl << endl;
+	
+	cout << "short ranges from " << SHRT_MIN;
+	cout << " up to " << SHRT_MAX;
+	
+	cout << endl << endl;
+	
+	cout << "unsigned short ranges up to " << USHRT_MAX;
+	
+	cout << endl << endl;
+	
+	cout << "int ranges from " << INT_MIN;
+	cout << " up to " << INT_MAX;
+	
+	cout << endl << endl;
+	
+	cout << "unsigned int ranges up to " << UINT_MAX;
+	
+	cout << endl << endl;
+	
+	cout << "long int range from " << LONG_MIN;
+	cout << " up to " << LONG_MAX;
+	
+	cout << endl << endl;
+	
+	cout << "unsigned long int ranges up to " << ULONG_MAX;
+	
+	cout << endl << endl;
+	
+	
+	cout << "long long int ranges from " << LLONG_MIN;
+	cout << " up to " << LLONG_MAX;
+	
+	cout << endl << endl;
+	
+	cout << "checking out the long long jazz: " << LONG_LONG_MAX;
+	
+	cout << endl << endl;
+	
+	cout << "unsigned long long int ranges up to " << ULLONG_MAX << endl;
+
+	return 0;
+}
