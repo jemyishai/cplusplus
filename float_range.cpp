59f3d79e92a79208c881d58cb635319411111c3a
jesse <jesse.moskowitz@gmail.com>
2020-08-17 08:35:52 -0400

    floating point

---
+++ float_range.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cfloat>
+
+using namespace std;
+
+int main ()
+{
+	cout << "float ranges from " << FLT_MIN;
+	cout << " up to " << FLT_MAX << endl;
+	
+	cout << endl << endl;
+	
+	cout << "double ranges from " << DBL_MIN;
+	cout << " up to "  << DBL_MAX << endl;
+	
+	return 0;
+}
